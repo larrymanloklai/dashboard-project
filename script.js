005269dd41c1a53da2ca9d1f7efc4b73a17c3093
larrymanloklai <larrymanlok.lai@gmail.com>
2024-08-27 20:13:24 +1000

    finished toggling the side bar with the content being shifted at the same time

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-document.getElementById('toggle-sidebar').addEventListener('click', function () {
+document.getElementById('toggle-sidebar').addEventListener('click', function() {
     document.querySelector('.sidebar').classList.toggle('collapsed');
+    document.querySelector('.main-content').classList.toggle('collapsed');
 });
